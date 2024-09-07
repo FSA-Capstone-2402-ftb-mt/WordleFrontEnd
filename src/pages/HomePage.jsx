4679bedfd68a0f4ff9f1f1b067ba1799d79892b7
Tyler Sloan <tcsloan44@gmail.com>
2024-09-07 10:50:17 -0400

    added some styling to the links on the home page to make them look nicer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,9 +6,9 @@ const HomePage = () => {
             <h1>Welcome to WordleGame!</h1>
             <p>Challenge your vocabulary and try to guess the word of the day. Play solo or compete against friends!</p>
             <div className="home-buttons">
-                <Link to="/games">Play Now</Link>
-                <Link to="/friends">Manage Friends</Link>
-                <Link to="/statistics">View Statistics</Link>
+                <Link to="/games" style={{ marginBottom: '10px', display: 'block' }}>Play Now</Link>
+                <Link to="/friends" style={{ marginBottom: '10px', display: 'block' }}>Manage Friends</Link>
+                <Link to="/statistics" style={{ marginBottom: '10px', display: 'block' }}>View Statistics</Link>
             </div>
         </div>
     );
