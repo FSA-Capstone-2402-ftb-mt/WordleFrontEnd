8dccd6381df3e99ff7ad620a2eb555546076a3de
Tyler Sloan <tcsloan44@gmail.com>
2024-09-08 20:58:10 -0400

    fixed bug where you could toggle the game version after submitting a guess on the standard version

--- src/components/Game/SwitchLabels.jsx
+++ src/components/Game/SwitchLabels.jsx
@@ -3,12 +3,13 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 
-export default function SwitchLabels({timerEnabled, setTimerEnabled}) {
+export default function SwitchLabels({timerEnabled, setTimerEnabled, disabled}) {
   return (
     <FormGroup>
       <FormControlLabel control={
         <Switch
             onChange={()=>{setTimerEnabled(!timerEnabled)}}
+            disabled={disabled}
         />} label="Timed Game Toggle" />
     </FormGroup>
   );
